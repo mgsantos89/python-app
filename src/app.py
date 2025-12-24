d4366759e7faf985ecfd92de386b9f69895da68c
Marco Gomes <marco@mgsantos.com.br>
2025-12-24 14:52:37 -0300

    Commit inicial

---
+++ src/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify
+import datetime
+import socket
+
+app = Flask(__name__)
+
+@app.route('/api/v1/details')
+
+def details():
+    return jsonify({
+        'time': datetime.datetime.now(),
+        'hostname' : socket.gethostname() 
+    })
+
+
+
+@app.route('/api/v1/healthz')
+
+def health():
+    return jsonify({
+        'status': 'UP'
+    }), 200
+
+
+if __name__ == '__main__':
+    # O parâmetro host='0.0.0.0' é a chave aqui
+    app.run(host='0.0.0.0', port=5000)
