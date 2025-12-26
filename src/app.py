4d092674622917077c209c4c7637f756be7ebff1
Marco Gomes <marco@mgsantos.com.br>
2025-12-26 16:09:44 -0300

    ajustes workflow

--- src/app.py
+++ src/app.py
@@ -10,7 +10,7 @@ def details():
     return jsonify({
         'time': datetime.datetime.now(),
         'hostname' : socket.gethostname(),
-        'message' :  'Esta tudo indo bem humanooide!!'
+        'message' :  'Esta tudo indo bem humanooide!a!'
     })
 
 
